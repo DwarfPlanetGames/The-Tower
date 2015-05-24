192a18a4b738680f720c4708ca4d0ebffd0c823d
brandon <brandon@brandon-AOA150>
2015-05-24 01:08:30 -0400

    Signed-off-by: brandon <brandon@brandon-AOA150> fixed artifact on side of screen... I think.

--- The Tower/src/com/DwarfPlanet/TheTower/GraphicsProcessing.java
+++ The Tower/src/com/DwarfPlanet/TheTower/GraphicsProcessing.java
@@ -8,7 +8,6 @@ import com.DwarfPlanet.TheTower.framework.BufferedImageLoader;
 
 public class GraphicsProcessing {
 	
-	public static BufferedImage image = BufferedImageLoader.loadImage("/128SpriteSheet.png");
 	public static int[] pixels;
 	public static BufferedImage img;
 	
@@ -26,9 +25,9 @@ public class GraphicsProcessing {
 	}
 	
 	public static void preBuffer() {
-		for (int x = -1; x < Game.width / 128 + 1; x++) {
+		for (int x = -1; x < Game.width / 128 + 2; x++) {
 			for (int y = -Game.height / 128 - 1; y < 1; y++) {
-				Draw.texture(-Game.camX % 128 + 128 * x, -Game.camY % 128 - 128 * y, 128, 128, image, 1, 0, true);
+				Draw.texture(-Game.camX % 128 + 128 * x, -Game.camY % 128 - 128 * y, 128, 128, Game.texture, 1, 0, true);
 			}
 		}
 	}
