08b94d1d590a4127e4a7cedc2713e9650768173d
brandon <brandon@brandon-AOA150>
2015-05-24 23:33:38 -0400

    Signed-off-by: brandon <brandon@brandon-AOA150> Added the class for witty remarks.

---
+++ The Tower/src/com/DwarfPlanet/TheTower/Objects/Remark.java
@@ -0,0 +1,18 @@
+package com.DwarfPlanet.TheTower.Objects;
+
+public class Remark {
+	
+	public static final String[] remarks = new String[]{
+		"This floor is so waxed I can just slide across the floor in the direction I'm looking!",
+		"People often tell me I look like a gorilla. Is that true?",
+		"#HighPolyTextures. Am I right?",
+		"I thing I found your NullPointerException, right... here.",
+		"Just to set the record straight, my name is Tracy",
+		"That is Tracy Garcia Bertha Bob Bill Lynda Gates Itchy Smith.",
+		"I am the Canine... Canine Lizard!",
+		"That's a really small planet. Like a Dwarf Planet.",
+		"I like the studio of red eyeballs the most bestest.",
+		"Someday I'll find my shoes!",
+	};
+	
+}
