9f96bc105fa399973adb93586a8a28d7fb1b76e8
brandon <brandon@brandon-AOA150>
2015-05-28 22:08:01 -0400

    Signed-off-by: brandon <brandon@brandon-AOA150>

--- The Tower/src/com/DwarfPlanet/TheTower/Objects/Remark.java
+++ The Tower/src/com/DwarfPlanet/TheTower/Objects/Remark.java
@@ -3,8 +3,10 @@ package com.DwarfPlanet.TheTower.Objects;
 public class Remark {
 	
 	public static final String[] remarks = new String[]{
+		"I found SCP-172! I'll go tell Dr.██████████",
 		"This floor is so waxed I can just slide in the direction I'm looking, without using my legs!",
 		"People often tell me I look like a gorilla. Is that true?",
+		"I found SCP-172! I'll go tell Dr.██████████",
 		"#HighPolyTextures. Am I right?",
 		"I thing I found your NullPointerException, right... here.",
 		"Just to set the record straight, my name is Tracy",
@@ -13,7 +15,8 @@ public class Remark {
 		"That's a really small planet. Like a Dwarf Planet.",
 		"I like the studio of red eyeballs the most bestest.",
 		"Someday I'll find my shoes!",
-		"These goggles help me see in the really bright sunlight here in this building."
+		"These goggles help me see in the really bright sunlight here in this building.",
+		"I found SCP-172! I'll go tell Dr.██████████",
 	};
 	
 }
